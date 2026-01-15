c013465f425a30238b7216d30c611fe103eb9925
Joey Collado <akosijoeyaibertcollado@gmail.com>
2026-01-15 16:00:13 +0800

    first commit

---
+++ good to know/how to create a project/readme.md
@@ -0,0 +1,7 @@
+1. find directory
+2. run in terminal "dotnet new console -n projectName"
+3. run in terminal to run program "dotnet run"
+
+
+Good website to learn = 'https://www.w3schools.com/cs/index.php?_gl=1*7sdn9t*_ga*MTYwMzIwMDUzMC4xNzY4NDYzNjQw*_ga_9YNMTB56NB*czE3Njg0NjM2MzkkbzEkZzEkdDE3Njg0NjM4ODIkajU0JGwwJGgw'
+
---
+++ variables/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x = 10;
+string name = "joey";
+bool isStudent = false;
+double Height = 6.7;
+char symbol = '#';
+
+Console.WriteLine(x);
+Console.WriteLine(name);
+Console.WriteLine(isStudent);
+Console.WriteLine(Height);
+Console.WriteLine(symbol);
+Console.WriteLine(x + Height);
